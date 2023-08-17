8885e01d7d7ddc549a5e8f9039145cd894cf962f
dagasfoka1 <129294699+dagasfoka1@users.noreply.github.com>
2023-08-17 14:52:16 -0300

    Update Exercicio2.c

--- Exercicio2.c
+++ Exercicio2.c
@@ -1,24 +1,51 @@
 #include <stdio.h>
 int i, maior, menor, x;
 int num[10];
-int main() {
-    for (i=0;i<10;i++){
-        printf("\nDigite de um número:\n");
-        scanf("%d",&num[i]);
-        if(i==0){
-            maior=num[i];
-            menor=num[i];
-        }
-        else{
-            if (num[i]>maior) {
-            maior=num[i];
-            }
-            if(num[i]<menor) {
-            menor=num[i];
-            }
-        }
+
+int
+main ()
+{
+  for (i = 0; i < 10; i++)
+    {
+      printf ("\nDigite de um nC:mero:\n");
+      scanf ("%d", &num[i]);
+      if (i == 0)
+	{
+	  maior = num[i];
+	  menor = num[i];
+	}
+      else
+	{
+	  if (num[i] > maior)
+	    {
+	      maior = num[i];
+	    }
+	  if (num[i] < menor)
+	    {
+	      menor = num[i];
+	    }
+	}
     }
-    printf("%d foi o maior e %d foi o menor digitado",maior, menor);
-    return 0;
- }
+
+  for (int i = 0; i < 10 - 1; i++)
+    {
+      for (int j = 0; j < 10 - i - 1; j++)
+	{
+	  if (num[j] > num[j + 1])
+	    {
+	      int aux = num[j];
+	      num[j] = num[j + 1];
+	      num[j + 1] = aux;
+	    }
+	}
+    }
+
+  printf ("%d foi o maior e %d foi o menor digitado", maior, menor);
+
+  for (int i = 0; i < 10; i++)
+    {
+      printf ("\n[%i] do Vetor = %i", i, num[i]);
+    }
+  return 0;
+}
 
